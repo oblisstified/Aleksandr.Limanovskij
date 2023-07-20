b4f0c72be0a42d20497c1b11b080e4e569851f92
k19079055 <PhantasmicEasrl1215>
2023-07-20 20:48:24 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import NavBar from "./components/NavBar";
+import background from "./assets/background.png";
+import linkedin from "./assets/linkedin.png";
+import github from "./assets/github.png";
+import email from "./assets/email.png";
+import "./App.css";
+import React, { useState } from "react";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <NavBar />
+      </div>
+      <div>
+        <img
+          style={{ flex: 1, width: "100%", height: "50%" }}
+          src={background}
+          alt="background"
+        />
+        <h1 className="header">Aleksandr Limanovskij</h1>
+      </div>
+
+      <div className="websites">
+        <a href="https://github.com/Oblisstified">
+          <img className="github" src={github} />
+        </a>
+        <a href="https://www.linkedin.com/in/aleksandr-limanovskij-0283b61a4/">
+          <img className="linkedin" src={linkedin} />
+        </a>
+        <a href="mailto:14alimanovskij@gmail.com">
+          <img className="email" src={email} />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
